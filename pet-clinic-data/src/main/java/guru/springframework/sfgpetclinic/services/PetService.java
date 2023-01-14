05ec368648a541ae6124f8f1a15c59a44906abf5
Manoj Marmat <manojrajput762000@gmail.com>
2023-01-14 16:54:43 +0530

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Pet;
+
 import java.util.Set;
 
 public interface PetService {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+
 import java.util.Set;
 
 public interface VetService {
